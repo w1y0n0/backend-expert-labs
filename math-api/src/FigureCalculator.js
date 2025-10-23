3dd91dd6e02d3c97aeb6d8547f6fe5516f5ebfa1
w1y0n0 <wiyono30@gmail.com>
2025-10-23 13:49:30 +0700

    Harus memiliki fungsi calculateRectanglePerimeter, calculateRectangleArea, calculateTrianglePerimeter, dan calculateTriangleArea

---
+++ math-api/src/FigureCalculator.js
@@ -0,0 +1,12 @@
+class FigureCalculator {
+    constructor(mathBasic) {
+        this._mathBasic = mathBasic;
+    }
+
+    calculateRectanglePerimeter() { }
+    calculateRectangleArea() { }
+    calculateTrianglePerimeter() { }
+    calculateTriangleArea() { }
+}
+
+module.exports = FigureCalculator;
