9e7f16a61618f13c292ef32d89f912bd3af747ad
w1y0n0 <wiyono30@gmail.com>
2025-10-23 15:48:54 +0700

    npm run lint & lint:fix

--- math-api/src/FigureCalculator.js
+++ math-api/src/FigureCalculator.js
@@ -1,49 +1,49 @@
 class FigureCalculator {
-    constructor(mathBasic) {
-        this._mathBasic = mathBasic;
+  constructor(mathBasic) {
+    this._mathBasic = mathBasic;
+  }
+
+  _verifyArgs(args, expectedArgsCount) {
+    if (args.length !== expectedArgsCount) {
+      throw new Error(`fungsi hanya menerima ${expectedArgsCount} parameter`);
     }
 
-    _verifyArgs(args, expectedArgsCount) {
-        if (args.length !== expectedArgsCount) {
-            throw new Error(`fungsi hanya menerima ${expectedArgsCount} parameter`);
-        }
+    args.forEach((arg) => {
+      if (typeof arg !== 'number') {
+        throw new Error('fungsi hanya menerima parameter number');
+      }
+    });
 
-        args.forEach((arg) => {
-            if (typeof arg !== 'number') {
-                throw new Error('fungsi hanya menerima parameter number');
-            }
-        });
+    return args;
+  }
 
-        return args;
-    }
+  calculateRectanglePerimeter(...args) {
+    const [length, width] = this._verifyArgs(args, 2);
 
-    calculateRectanglePerimeter(...args) {
-        const [length, width] = this._verifyArgs(args, 2);
+    // formula: (2 * (length + width))
+    return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
+  }
 
-        // formula: (2 * (length + width))
-        return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
-    }
+  calculateRectangleArea(...args) {
+    const [length, width] = this._verifyArgs(args, 2);
 
-    calculateRectangleArea(...args) {
-        const [length, width] = this._verifyArgs(args, 2);
+    // formula: (length * width)
+    return this._mathBasic.multiply(length, width);
+  }
 
-        // formula: (length * width)
-        return this._mathBasic.multiply(length, width);
-    }
+  calculateTrianglePerimeter(...args) {
+    const [sideA, sideB, base] = this._verifyArgs(args, 3);
 
-    calculateTrianglePerimeter(...args) {
-        const [sideA, sideB, base] = this._verifyArgs(args, 3);
+    // formula: (sideA + sideB + base)
+    return this._mathBasic.add(sideA, this._mathBasic.add(sideB, base));
+  }
 
-        // formula: (sideA + sideB + base)
-        return this._mathBasic.add(sideA, this._mathBasic.add(sideB, base));
-    }
+  calculateTriangleArea(...args) {
+    const [base, height] = this._verifyArgs(args, 2);
 
-    calculateTriangleArea(...args) {
-        const [base, height] = this._verifyArgs(args, 2);
-
-        // formula: ((base * height) / 2)
-        return this._mathBasic.divide(this._mathBasic.multiply(base, height), 2);
-    }
+    // formula: ((base * height) / 2)
+    return this._mathBasic.divide(this._mathBasic.multiply(base, height), 2);
+  }
 }
 
 module.exports = FigureCalculator;
