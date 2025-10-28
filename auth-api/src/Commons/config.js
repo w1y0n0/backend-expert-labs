be5ae15ddd2a7d1a8e2810f5bc624a04e489dfbe
w1y0n0 <wiyono30@gmail.com>
2025-10-28 08:46:52 +0700

    Membuat HTTP Server dan Functional Test

--- auth-api/src/Commons/config.js
+++ auth-api/src/Commons/config.js
@@ -8,8 +8,13 @@ if (process.env.NODE_ENV === 'test') {
 } else {
     dotenv.config();
 }
-
+/* istanbul ignore file */
 const config = {
+    app: {
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        port: process.env.PORT,
+        debug: process.env.NODE_ENV === 'development' ? { request: ['error'] } : {},
+    },
     database: {
         host: process.env.PGHOST,
         port: process.env.PGPORT,
