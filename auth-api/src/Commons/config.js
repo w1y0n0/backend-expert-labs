f96cfa76be31684c8c76565c319adb0f435171c2
w1y0n0 <wiyono30@gmail.com>
2025-10-29 09:19:37 +0700

    Implement JwtTokenManager for JWT handling and add corresponding tests

--- auth-api/src/Commons/config.js
+++ auth-api/src/Commons/config.js
@@ -1,14 +1,15 @@
+/* istanbul ignore file */
 const dotenv = require('dotenv');
 const path = require('path');
 
 if (process.env.NODE_ENV === 'test') {
   dotenv.config({
-    path: path.resolve(process.cwd(), '.test.env')
+    path: path.resolve(process.cwd(), '.test.env'),
   });
 } else {
   dotenv.config();
 }
-/* istanbul ignore file */
+
 const config = {
   app: {
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -21,6 +22,12 @@ const config = {
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
+  },
+  auth: {
+    jwtStrategy: 'forumapi',
+    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+    refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
+    accessTokenAge: process.env.ACCESS_TOKEN_AGE,
   },
 };
 
