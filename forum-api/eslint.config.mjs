62329687c846a6a7113a3e0f4164accdb16223c6
w1y0n0 <wiyono30@gmail.com>
2025-11-02 09:38:05 +0700

    starter project

---
+++ forum-api/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default defineConfig([
+  daStyle,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.test.js', '**/__tests__/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  }
+]);
